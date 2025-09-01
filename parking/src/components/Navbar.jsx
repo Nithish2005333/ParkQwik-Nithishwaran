37f4fb499bf09efd586b00398283da1ccc8e7c8e
Nithish2005333 <nithishwaran2005333@gmail.com>
2025-09-01 20:15:36 +0530

    Fix bugs

--- parking/src/components/Navbar.jsx
+++ parking/src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { Menu, X } from "lucide-react";
 
+import logo from "../assets/logo1.png";
+
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -20,10 +22,11 @@ const Navbar = () => {
       <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-between h-20">
         {/* Logo */}
         <div className="flex items-center gap-2">
-          <img src="/logo1.png" alt="Park Qwik Logo" className="h-18 w-36" />
+          <img src={logo} alt="Park Qwik Logo" className="h-16 w-36 object-contain" />
         </div>
+
         {/* Desktop Menu */}
-        <ul className="hidden md:flex space-x-8 text- font-medium text-gray-700">
+        <ul className="hidden md:flex space-x-8 font-medium text-gray-700">
           {navItems.map((item) => (
             <li key={item.name}>
               <NavLink
@@ -32,7 +35,8 @@ const Navbar = () => {
                   isActive
                     ? "text-green-600 border-b-2 border-green-600 pb-1"
                     : "hover:text-green-600 transition-colors duration-200"
-                }>
+                }
+              >
                 {item.name}
               </NavLink>
             </li>
@@ -43,7 +47,8 @@ const Navbar = () => {
         <div className="md:hidden">
           <button
             onClick={() => setMenuOpen(!menuOpen)}
-            className="text-gray-700 focus:outline-none">
+            className="text-gray-700 focus:outline-none"
+          >
             {menuOpen ? <X size={24} /> : <Menu size={24} />}
           </button>
         </div>
@@ -62,7 +67,8 @@ const Navbar = () => {
                     isActive
                       ? "block text-green-600"
                       : "block hover:text-green-600"
-                  }>
+                  }
+                >
                   {item.name}
                 </NavLink>
               </li>
